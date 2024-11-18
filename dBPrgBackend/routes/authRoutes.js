3ff87fc1c2a3d4b02de4f76039e98325e9fd0730
srujanaayy <131008131+srujanaayy@users.noreply.github.com>
2024-11-18 08:29:22 -0600

    Update authRoutes.js

--- dBPrgBackend/routes/authRoutes.js
+++ dBPrgBackend/routes/authRoutes.js
@@ -1,41 +1,6 @@
-// const express = require("express");
-// const router = express.Router();
-// const authController = require("../controllers/authController");
-// const authValidators = require("../utils/validators/auth");
-// const { validate } = require("express-validator");
-
-// // Registration route with validation
-// router.post(
-//   "/register",
-//   authValidators.register,
-//   (req, res, next) => {
-//     const errors = validate(req);
-//     if (!errors.isEmpty()) {
-//       return res.status(400).json({ errors: errors.array() });
-//     }
-//     next();
-//   },
-//   authController.register
-// );
-
-// // Login route with validation
-// router.post(
-//   "/login",
-//   authValidators.login,
-//   (req, res, next) => {
-//     const errors = validate(req);
-//     if (!errors.isEmpty()) {
-//       return res.status(400).json({ errors: errors.array() });
-//     }
-//     next();
-//   },
-//   authController.login
-// );
-
-// module.exports = router;
 const express = require("express");
 const router = express.Router();
-const authController = require("./authController");
+const authController = require("../controllers/authController");
 const authValidators = require("../utils/validators/auth");
 const { validationResult } = require("express-validator");
 
