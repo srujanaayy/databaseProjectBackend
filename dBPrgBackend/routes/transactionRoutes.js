b0140a9450aa089f6cd6f19c72488537cefb249a
srujanaayy <131008131+srujanaayy@users.noreply.github.com>
2024-12-10 16:00:27 -0600

    Delete dBPrgBackend directory

--- dBPrgBackend/routes/transactionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const transactionController = require("./transactionController");
-
-// Define the POST route for creating an order
-router.post("/create-order", transactionController.createOrder);
-
-module.exports = router;
